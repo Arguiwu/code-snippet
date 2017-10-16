677e6ae42baf30003f747ad4fdfb558840856ba0
vinson <wxgsorry@gmail.com>
2017-10-16 09:45:52 +0800

    :rocket: add test files

---
+++ test/2017-10-16-1.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+
+function example(a, b) {
+    return a + b
+}
+
+//example(1, true)
+example(1, 1)
---
+++ test/README.md
@@ -0,0 +1,3 @@
+一些随手测试DEMO
+
+### [不编译使用typescript类型检测](https://github.com/Arguiwu/code-snippet/blob/master/test/2017-10-16-1.js)
