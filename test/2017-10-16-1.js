3d4d26a0eab5cfc20af15dd1c65b56f06aa91af6
vinson <wxgsorry@gmail.com>
2017-10-16 10:05:56 +0800

    :beer: update test ts-check

--- test/2017-10-16-1.js
+++ test/2017-10-16-1.js
@@ -10,5 +10,26 @@ function example(a, b) {
     return a + b
 }
 
-//example(1, true)
-example(1, 1)
+example(1, true)    // ✖
+example(1, 1)       // ✔
+
+/**
+ * @typedef {Object} Issue
+ * @property {string} url
+ * @property {string} repository_url
+ * @property {number} id
+ * @property {string} title
+ * @property {string} state
+ * @property {bool} open
+ */
+
+ const url = "https://api.github.com/repos/microsoft/typescript/issues"
+
+ (async () => {
+     let response = await getComputedStyle(url)
+     /** @type {Issue[]} */
+     let issues = response.body
+     for (let issue of issues) {
+         console.log(issue.title)
+     }
+ })()
