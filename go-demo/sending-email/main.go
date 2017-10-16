481e45238c330208298d61626dcdf72fffb66ef8
vinson <wxgsorry@gmail.com>
2017-10-16 17:24:07 +0800

    :zap: add email-template

--- go-demo/sending-email/main.go
+++ go-demo/sending-email/main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"bytes"
 	"gomail"
+	"html/template"
 )
 
 func main() {
 
+	t, _ := template.ParseFiles("email-tmplate.html")
+
+	buffer := new(bytes.Buffer)
+
+	t.Execute(buffer, struct {
+		Login string
+	}{
+		Login: "马上去登录",
+	})
+
+	body := buffer.String()
+
 	m := gomail.NewMessage()
 	m.SetAddressHeader("From", "xxx@qq.com", "admin")
-	m.SetHeader("To", 
+	m.SetHeader("To",
 		m.FormatAddress("xxx@qq.com", "测试"),
 	)
 	m.SetHeader("Subject", "Gomail")
 
-	m.SetBody("text/html", "测试")
+	m.SetBody("text/html", body)
 
 	d := gomail.NewPlainDialer("smtp.qq.com", 465, "xxx@qq.com", "nubhrudbbtlgdcee")
 
